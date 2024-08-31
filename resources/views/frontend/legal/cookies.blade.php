6e0690c092dbc937fe8f4621dfa7107d3753f0bd
Adnankhan099 <adnankhan0999@gmail.com>
2024-08-31 22:18:52 +0500

    first commit

---
+++ resources/views/frontend/legal/cookies.blade.php
@@ -0,0 +1,157 @@
+<x-guest-layout>
+    <section class="container-fluid px-lg-5 py-4 px-md-3 px-3">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{ route('home') }}" class="text-uppercase">Home</a></li>
+                <li class="breadcrumb-item"><a class="text-uppercase">Legal Pages</a></li>
+            </ol>
+        </nav>
+
+        <div class="row">
+            <div class="col-12">
+                <h1 class="fs-1 text-dark text-uppercase fw-bolder">
+                    Cookies Policy
+                </h1>
+            </div>
+        </div>
+    </section>
+
+    <section class="container-fluid px-lg-5 px-md-3 px-3 py-lg-5 py-3" style="background-color: #f0f0f0;">
+        <div class="row">
+            <div class="col-12">
+                <p>
+                    At KitchenKit (“us” or “we”), we want to ensure that your visit to our website, (the “Website”) is
+                    smooth, reliable and as useful to you as possible. Subject to your consent where required by
+                    applicable law, we use cookies and similar technologies (“cookies”) to help us improve the use and
+                    functionality of the KitchenKit Website and to better understand how visitors use our Website and
+                    the tools and services offered on it.
+                </p>
+
+                <h2 class="text-dark fw-bolder text-uppercase py-1">What are cookies?</h2>
+
+                <p class="py-1">
+                    A “cookie” is a small piece of data that may be stored on your computer or mobile device. Cookies
+                    serve a number of purposes like letting you navigate between pages efficiently, remembering your
+                    preferences, interests or log in details, and generally improving your experience. We and some of
+                    our partners also use cookies on our Website to measure the effectiveness of advertising and how
+                    visitors use our Website. The use of cookies on the Website allows you to enjoy more seamless visits
+                    and more accurately measures your behaviour on the Website.
+                </p>
+
+                <p class="py-1">
+                    Most web browsers automatically accept cookies, but you can usually prevent accepting cookies by
+                    changing your browser settings. Cookies help us give you a better website, by letting us monitor
+                    what’s working and what isn’t through site traffic analysis.
+                    We use 5 cookie categories on the BA website:
+                </p>
+            </div>
+
+            <div class="col-12">
+                <table class="table table-striped">
+                    <thead>
+                        <th class="text-uppercase">Category</th>
+                        <th class="text-uppercase">EXPLANATION</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Essential</td>
+                            <td>We set these cookies in order to maintain the website functionality and integrity of
+                                your visit and the contents of your shopping basket. Not setting these cookies means the
+                                site may not function correctly for you</td>
+                        </tr>
+                        <tr>
+                            <td>Customer</td>
+                            <td>We use these cookies to remember that you have visited our site before, so we can
+                                improve your experience by showing you the most relevant products and offers.
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Analytics</td>
+                            <td>We use these cookies to aggregate information about how our visitors and customers use
+                                our site, this allows us to improve our site and make it better for everyone.
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Social Networks</td>
+                            <td>We use cookies from companies like Facebook, in order for their users to get the most
+                                out of visiting our site.
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Adverts on other websites </td>
+                            <td>When we advertise on other websites, cookies from advertising networks tailor those
+                                adverts to you, showing you KitchenKit products we believe you will find relevant and
+                                interesting
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="col-12">
+                <h3 class="fw-bolder text-uppercase text-dark py-1">ADVERTS ON OTHER WEBSITES
+                </h3>
+                <p class="py-1">
+                    When we advertise on other websites, cookies from advertising networks tailor those adverts to you,
+                    showing you KitchenKit products we believe you will find relevant and interesting
+                </p>
+
+                <h2 class="fw-bolder text-uppercase text-dark py-1">HOW TO MANAGE AND DISABLE COOKIES
+                </h2>
+
+                <p class="py-1">
+                    Most browsers automatically accept cookies, but you can usually change your browser to prevent
+                    cookies being stored. Please note, if you do turn cookies off this will limit the service that we
+                    are able to provide to you and may affect your visitor experience. You can instruct your browser, by
+                    changing its options to stop accepting cookies or to prompt you before accepting a cookie from the
+                    website you visit. Our system will automatically issue cookies when you log in to the Website unless
+                    you have adjusted your browser setting so that it will refuse cookies. www.allaboutcookies.org
+                    provides good simple instructions on how to manage cookies on the different types of web browsers.
+                </p>
+
+                <p class="py-1">
+                    By using the Website, you agree that we can place these types of cookies on your device. If you do
+                    not want these cookies on your device, you can change your cookie settings at any time through your
+                    web browser settings. Here are the locations of the cookie settings for all major web browsers:
+                </p>
+
+                <ul class="p-0">
+                    <li class="my-1"><i class="ri-arrow-right-s-fill"></i> Internet Explorer – Tools > Internet
+                        Options > Privacy
+                        tab.
+                    </li>
+                    <li class="my-1"><i class="ri-arrow-right-s-fill"></i> Mozilla Firefox – Tools > Options > Privacy
+                        menu.
+                    </li>
+                    <li class="my-1"><i class="ri-arrow-right-s-fill"></i> Safari Users – Edit > Preferences > Privacy
+                        menu.
+                    </li>
+                    <li class="my-1"><i class="ri-arrow-right-s-fill"></i> Chrome Users – Settings > Content Settings
+                        > Privacy > Cookies.
+                    </li>
+                </ul>
+
+                <p class="py-1">
+                    To block Google Analytics cookies specifically, you can install the “Google Analytics Opt-out
+                    Browser Add-on” provided by Google.
+                </p>
+
+                <h2 class="fw-bolder text-uppercase text-dark py-1">CHANGES TO THIS COOKIE POLICY
+                </h2>
+
+                <p class="py-1">We can change this Cookie Policy at any time. If we make material changes to the
+                    Cookie Policy, we
+                    will let you know either by posting the changed Cookie Policy on the Site or by sending you an
+                    email.
+                </p>
+                <p class="py-1">
+                    It’s important that you review the changed Cookie Policy. If you do not wish to agree to the changed
+                    Cookie Policy, then we cannot continue to provide the Website to you, and your only option is to
+                    stop accessing the Website.
+                </p>
+
+            </div>
+        </div>
+    </section>
+
+</x-guest-layout>
