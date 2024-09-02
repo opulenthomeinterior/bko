c813a945af3ab3ad01be1a7ed798007993d9fa43
opulenthomeinterior <buykitchenonline@gmail.com>
2024-09-02 00:37:47 +0000

    Initial commit

---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,93 @@
+<x-guest-layout>
+    <section class="container-fluid bg-dark px-lg-5 py-4 px-md-3 px-3">
+        <div class="row py-4">
+            <div class="col-12 text-center">
+                <h1 class="fs-1 text-white text-uppercase fw-bolder">
+                    KITCHEN KIT. TRADE KITCHENS
+                </h1>
+                <a href="{{ route('orderkitchen') }}" class="btn btn-md bg-white fw-bolder text-uppercase rounded-0"
+                    href="{{ route('orderkitchen') }}">Order Now</a>
+            </div>
+        </div>
+    </section>
+
+    <section class="container-fluid px-lg-5 px-md-3 px-3 py-lg-5 py-3" style="background-color: #f0f0f0;">
+        <div class="row">
+            <div class="col-lg-6 text-center">
+                <h1 class="text-uppercase text-dark fw-bolder" style="font-size: 4rem">20% OFF </h1>
+                <h3 class="text-uppercase fw-bolder text-dark">GET 20% OFF ALL ORDERS WHEN YOU OPEN A TRADE ACCOUNT</h3>
+            </div>
+            <div class="col-lg-6">
+                <ul>
+                    <li class="fw-bold py-1"><i class="ri-check-line"></i>
+                        Kitchen despatched in 48 hours
+                    </li>
+                    <li class="fw-bold py-1"><i class="ri-check-line"></i>
+                        Over 30 years manufacturing in the UK
+                    </li>
+                    <li class="fw-bold py-1"><i class="ri-check-line"></i>
+                        Standard or made to measure
+                    </li>
+                    <li class="fw-bold py-1"><i class="ri-check-line"></i>
+                        25 years quality guarantee on all cabinets
+                    </li>
+                    <li class="fw-bold py-1"><i class="ri-check-line"></i>
+                        FIRA Gold Certified
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
+    <section class="container-fluid position-relative"
+        style="background-image: url('{{ asset('images/Slab-Kitchen.jpg') }}'); background-position: center; background-repeat: no-repeat; background-size: cover; height: 70vh;">
+        <div class="row">
+            <div class="col-12 text-center text-over-image">
+                <h1 class="text-white fw-bolder" style="text-shadow: 1px 1px 3px black;">WE CAN'T BE BEATEN ON PRICE!
+                </h1>
+                <a href="{{ route('orderkitchen') }}"
+                    class="btn btn-md bg-dark fw-bolder text-white text-uppercase rounded-0">Shop now</a>
+            </div>
+        </div>
+
+    </section>
+
+    <section class="container-fluid px-lg-5 px-md-3 px-3 py-lg-5 py-3" style="background-color: #f0f0f0;">
+        <div class="row">
+            <div class="col-12">
+                <p>
+                    Kitchen Kit is a Kitchen trade brand of BA operating in the Merchant and e-Commerce trade channels.
+                    <br><br>
+                    BA is proud to be one of the UK’s leading manufacturers of factory-built kitchens and bedrooms,
+                    bespoke, made-to-measure doors and one of the largest ranges of matching accessories in the market
+                    place today.
+                    <br><br>
+                    BA’s knowledge and passion for kitchens, bedrooms and components enables our customers to keep the
+                    promises they make to their clients. Every product is crafted with pride by our skilled workforce in
+                    our UK factories to provide years of enjoyment.
+                    <br><br>
+                    Continuous investment in sustainable manufacturing, design and innovation ensures that our customers
+                    can be confident of always staying ahead. Our dedicated, agile teams deliver on every front, every
+                    time.
+                    <br><br>
+                    Born out of a desire to offer better products and service in the market, BA was established in 1990
+                    and has gone from strength to strength since. BA operates from three state of the art factories in
+                    Cookstown, Co. Tyrone and two in Yorkshire at Rotherham and Doncaster.
+                    <br><br>
+                    At Kitchen Kit which operates from our Doncaster site serving the UK & Ireland, we aim to make the
+                    journey of ordering your kitchen as easy as possible; from support with kitchen design and quotes,
+                    pre-selecting the most on trend colours and styles, ensuring our products are easy to buy with the
+                    hinge, door and cabinet all on one sku with only one pric, to ensuring our units are easy to build
+                    with clic technology and guaranteeing fast delivery times. From order to delivery you can receive
+                    your flat-pack kitchen in 3 working days (UK Mainland). We also offer pre-assembled units or rigid
+                    cabinets on a 10 working day lead time until despatch. Standard sizes and made to measure available
+                    across our J-Pull Handleless Kitchens and Slab Kitchens.
+                </p>
+            </div>
+        </div>
+    </section>
+
+
+
+
+</x-guest-layout>
