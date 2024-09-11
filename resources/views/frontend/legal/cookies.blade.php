daf9e6af3f5c415710156190c47fdb148c52fbe2
taimoor5356 <mtaimoornaveed@gmail.com>
2024-09-11 22:26:36 +0500

    updated cookies policy

--- resources/views/frontend/legal/cookies.blade.php
+++ resources/views/frontend/legal/cookies.blade.php
@@ -20,29 +20,17 @@
         <div class="row">
             <div class="col-12">
                 <p>
-                    At KitchenKit (“us” or “we”), we want to ensure that your visit to our website, (the “Website”) is
-                    smooth, reliable and as useful to you as possible. Subject to your consent where required by
-                    applicable law, we use cookies and similar technologies (“cookies”) to help us improve the use and
-                    functionality of the KitchenKit Website and to better understand how visitors use our Website and
-                    the tools and services offered on it.
+                    At Opulent Home Interiors (“us” or “we”), our goal is to ensure that your experience on our website (the “Website”) is smooth, reliable, and as beneficial to you as possible. With your consent where required by applicable law, we use cookies and similar technologies (“cookies”) to enhance the functionality and usability of the Opulent Home Interiors Website and to gain a better understanding of how visitors interact with our Website and the tools and services we provide.
                 </p>
 
                 <h2 class="text-dark fw-bolder text-uppercase py-1">What are cookies?</h2>
 
                 <p class="py-1">
-                    A “cookie” is a small piece of data that may be stored on your computer or mobile device. Cookies
-                    serve a number of purposes like letting you navigate between pages efficiently, remembering your
-                    preferences, interests or log in details, and generally improving your experience. We and some of
-                    our partners also use cookies on our Website to measure the effectiveness of advertising and how
-                    visitors use our Website. The use of cookies on the Website allows you to enjoy more seamless visits
-                    and more accurately measures your behaviour on the Website.
+                A “cookie” is a small piece of data that can be stored on your computer or mobile device. Cookies serve several purposes, such as allowing you to navigate between pages efficiently, remembering your preferences, interests, or login details, and generally enhancing your overall experience. We, along with some of our partners, also use cookies on our Website to measure the effectiveness of advertising and understand how visitors use our Website. By using cookies, we can ensure your visits are more seamless and accurately assess your behaviour on the Website
                 </p>
 
                 <p class="py-1">
-                    Most web browsers automatically accept cookies, but you can usually prevent accepting cookies by
-                    changing your browser settings. Cookies help us give you a better website, by letting us monitor
-                    what’s working and what isn’t through site traffic analysis.
-                    We use 5 cookie categories on the BA website:
+                Most web browsers automatically accept cookies, but you can usually modify your browser settings to decline cookies if you prefer. Cookies help us provide you with a better Website experience by allowing us to monitor what works well and what needs improvement through site traffic analysis. We use 5 cookie categories on the BA website:
                 </p>
             </div>
 
@@ -55,33 +43,26 @@
                     <tbody>
                         <tr>
                             <td>Essential</td>
-                            <td>We set these cookies in order to maintain the website functionality and integrity of
-                                your visit and the contents of your shopping basket. Not setting these cookies means the
-                                site may not function correctly for you</td>
+                            <td>These cookies are crucial for maintaining the functionality of our website and ensuring the integrity of your visit, including the contents of your shopping basket. Disabling these cookies may result in the site not functioning correctly for you</td>
                         </tr>
                         <tr>
                             <td>Customer</td>
-                            <td>We use these cookies to remember that you have visited our site before, so we can
-                                improve your experience by showing you the most relevant products and offers.
+                            <td>These cookies help us remember that you’ve visited our site before, allowing us to enhance your experience by displaying the most relevant products and offers tailored to your preferences.
                             </td>
                         </tr>
                         <tr>
                             <td>Analytics</td>
-                            <td>We use these cookies to aggregate information about how our visitors and customers use
-                                our site, this allows us to improve our site and make it better for everyone.
+                            <td>We use these cookies to gather aggregated information on how our visitors and customers use our site. This data enables us to continually improve our site, making it better for everyone
                             </td>
                         </tr>
                         <tr>
                             <td>Social Networks</td>
-                            <td>We use cookies from companies like Facebook, in order for their users to get the most
-                                out of visiting our site.
+                            <td>We use cookies from companies like Facebook to enhance the experience for their users when they visit our site.
                             </td>
                         </tr>
                         <tr>
                             <td>Adverts on other websites </td>
-                            <td>When we advertise on other websites, cookies from advertising networks tailor those
-                                adverts to you, showing you KitchenKit products we believe you will find relevant and
-                                interesting
+                            <td>When we advertise on other websites, cookies from advertising networks help tailor those ads to you, showcasing Opulent Home Interiors products that we believe you will find relevant and interesting.
                             </td>
                         </tr>
                     </tbody>
@@ -92,27 +73,18 @@
                 <h3 class="fw-bolder text-uppercase text-dark py-1">ADVERTS ON OTHER WEBSITES
                 </h3>
                 <p class="py-1">
-                    When we advertise on other websites, cookies from advertising networks tailor those adverts to you,
-                    showing you KitchenKit products we believe you will find relevant and interesting
+                When we advertise on other websites, cookies from advertising networks tailor those ads to you, showcasing Opulent Home Interiors products that we believe you will find relevant and interesting.
                 </p>
 
                 <h2 class="fw-bolder text-uppercase text-dark py-1">HOW TO MANAGE AND DISABLE COOKIES
                 </h2>
 
                 <p class="py-1">
-                    Most browsers automatically accept cookies, but you can usually change your browser to prevent
-                    cookies being stored. Please note, if you do turn cookies off this will limit the service that we
-                    are able to provide to you and may affect your visitor experience. You can instruct your browser, by
-                    changing its options to stop accepting cookies or to prompt you before accepting a cookie from the
-                    website you visit. Our system will automatically issue cookies when you log in to the Website unless
-                    you have adjusted your browser setting so that it will refuse cookies. www.allaboutcookies.org
-                    provides good simple instructions on how to manage cookies on the different types of web browsers.
+                Most browsers automatically accept cookies, but you can typically modify your browser settings to prevent cookies from being stored. Please be aware that if you disable cookies, it may limit the services we can offer you and could impact your experience on our Website. You can instruct your browser to stop accepting cookies or to prompt you before accepting a cookie from the websites you visit by adjusting its settings. Unless you’ve configured your browser to refuse cookies, our system will automatically issue them when you log in to the Website. For simple instructions on how to manage cookies across different web browsers, you can visit www.allaboutcookies.org.
                 </p>
 
                 <p class="py-1">
-                    By using the Website, you agree that we can place these types of cookies on your device. If you do
-                    not want these cookies on your device, you can change your cookie settings at any time through your
-                    web browser settings. Here are the locations of the cookie settings for all major web browsers:
+                By using the Website, you consent to us placing these types of cookies on your device. If you prefer not to have these cookies on your device, you can adjust your cookie settings at any time through your web browser settings. Below are the locations of cookie settings for major web browsers
                 </p>
 
                 <ul class="p-0">
@@ -132,22 +104,16 @@
                 </ul>
 
                 <p class="py-1">
-                    To block Google Analytics cookies specifically, you can install the “Google Analytics Opt-out
-                    Browser Add-on” provided by Google.
+                To specifically block Google Analytics cookies, you can install the “Google Analytics Opt-out Browser Add-on” provided by Google.
                 </p>
 
                 <h2 class="fw-bolder text-uppercase text-dark py-1">CHANGES TO THIS COOKIE POLICY
                 </h2>
 
-                <p class="py-1">We can change this Cookie Policy at any time. If we make material changes to the
-                    Cookie Policy, we
-                    will let you know either by posting the changed Cookie Policy on the Site or by sending you an
-                    email.
+                <p class="py-1">We reserve the right to update this Cookie Policy at any time. If we make significant changes, we will inform you by either posting the updated Cookie Policy on the Website or by sending you an email.
                 </p>
                 <p class="py-1">
-                    It’s important that you review the changed Cookie Policy. If you do not wish to agree to the changed
-                    Cookie Policy, then we cannot continue to provide the Website to you, and your only option is to
-                    stop accessing the Website.
+                It's important that you review the updated Cookie Policy. If you do not agree with the changes, we regret that we won't be able to continue providing access to the Website, and your only option would be to discontinue using the Website.
                 </p>
 
             </div>
