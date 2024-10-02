892382f87660a5fc76d4026c9f593f65a4fc6800
taimoor5356 <mtaimoornaveed@gmail.com>
2024-10-03 00:55:14 +0500

    updated

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -57,7 +57,7 @@
         </div>
     </section> -->
 
-    <section class="container-fluid position-relative" style="height: 70vh; overflow: hidden; border-bottom: 7px solid #febd49; border-right: 5px solid #febd49">
+    <section class="container-fluid position-relative" style="height: 70vh; overflow: hidden; border-bottom: 5px solid #febd49; border-left: 5px solid #febd49">
         <img src="{{ asset('images/Slab-Kitchen.jpg') }}"
             alt="Bespoke Kitchen Units in London and Surroundings"
             style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover; object-position: center; z-index: -1;">
