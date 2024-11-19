de5128fc45b0e0923a8b026108680f9aa20bcfcc
taimoor5356 <mtaimoornaveed@gmail.com>
2024-11-20 03:32:06 +0500

    updated

--- resources/views/pages/styles/edit.blade.php
+++ resources/views/pages/styles/edit.blade.php
@@ -65,7 +65,7 @@
                                         @if (count($style->testimonials) < 5)
                                             <div class="card border border-default p-3">
                                                 <label for="" class="form-label">
-                                                    Testimonial {{$key+1}}
+                                                    Testimonial {{count($style->testimonials)+1}}
                                                 </label>
                                                 <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2">
                                                     <input type="date" name="date[]" class="form-control">
@@ -75,7 +75,7 @@
                                                 <input type="text" name="user_name[]" class="form-control" placeholder="Enter User Name">
                                                 <label for="" class="form-label">
                                                 </label>
-                                                <textarea name="testimonial[]" class="form-control" placeholder="Enter Testimonial"><?= str_replace('&', '&', $testimonial->testimonial) ?></textarea>
+                                                <textarea name="testimonial[]" class="form-control" placeholder="Enter Testimonial"></textarea>
                                             </div>
                                         @endif
                                     @else
