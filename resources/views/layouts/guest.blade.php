57aa7e9cb0ed607529ffca82dde3417aa387f5cd
taimoor5356 <mtaimoornaveed@gmail.com>
2024-12-19 17:50:04 +0500

    updated prices with brackets

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1034,6 +1034,7 @@ use App\Models\Style;
 
         $(document).ready(function() {
             $('.select-2').select2({
+                placeholder: "Search",
                 dropdownCssClass: 'custom-select-dropdown',
                 templateResult: function (data) {
                     if (!data.id) {
@@ -1135,6 +1136,7 @@ use App\Models\Style;
             $('button[data-bs-toggle="tab"]').on('shown.bs.tab', function (e) {
                 // Select the dropdown in the currently active tab
                 $('.tab-pane.active .select-2').select2({
+                    placeholder: "Search",
                     dropdownCssClass: 'custom-select-dropdown',
                     templateResult: function (data) {
                         if (!data.id) {
@@ -1162,6 +1164,7 @@ use App\Models\Style;
             $('.collapse-container').on('shown.bs.collapse', function () {
                 // Initialize Select2 within the visible collapse section
                 $(this).find('.select-2').select2({
+                    placeholder: "Search",
                     dropdownCssClass: 'custom-select-dropdown',
                     templateResult: function (data) {
                         if (!data.id) {
