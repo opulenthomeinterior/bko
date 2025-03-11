89f133770180510cc15a69f11182f5bc9b98fd96
taimoor5356 <mtaimoornaveed@gmail.com>
2025-03-12 03:58:54 +0500

    updated

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -119,7 +119,7 @@
     </section>
 
     <!-- ORDER KITCHEN-->
-    <section class="container-fluid py-5 bg-white" style="border-bottom: 3px solid #ebc266; border-left: 3px solid #ebc266">
+    <section class="container-fluid py-5 bg-white" style="border-bottom: 3px solid #ebc266; border-left: 3px solid #ebc266; width: 100% !important; overflow: hidden">
         <div class="row">
             <h3 class="text-uppercase fw-bolder text-dark text-center">ORDER KITCHEN</h3>
         </div>
@@ -684,56 +684,6 @@
             // Customize the autoplay behavior to reverse the direction
             $carousel01.on('translated.owl.carousel', function() {
                 $carousel01.find('.owl-item.active').css('animation', 'move-right 0.3s ease-in-out');
-            });
-            
-            const $carousel02 = $('.main-carousel-banner-02');
-            // Initialize OwlCarousel
-            $carousel02.owlCarousel({
-                loop: true,
-                margin: 30,
-                stagePadding: 15, // Add padding to avoid clipping
-                rtl: false,
-                autoplay: true,
-                autoplayTimeout: 3000,
-                autoplayHoverPause: true,
-                responsive: {
-                    0: {
-                        items: 1,
-                        nav: true
-                    },
-                    768: {
-                        items: 2,
-                        nav: true
-                    },
-                    992: {
-                        items: 3,
-                        loop: true,
-                        margin: 10,
-                        nav: true,
-                        dots: true,
-                        center: true,
-                    },
-                    1200: {
-                        items: 4,
-                        loop: true,
-                        margin: 20,
-                        nav: true,
-                        dots: true,
-                        center: true,
-                    },
-                    1400: {
-                        items: 4,
-                        loop: true,
-                        margin: 20,
-                        nav: true,
-                        dots: true,
-                        center: true,
-                    }
-                }
-            });
-            // Customize the autoplay behavior to reverse the direction
-            $carousel02.on('translated.owl.carousel', function() {
-                $carousel02.find('.owl-item.active').css('animation', 'move-left 0.3s ease-in-out');
             });
         });
     </script>
