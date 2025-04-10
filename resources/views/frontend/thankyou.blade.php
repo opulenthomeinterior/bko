277820bf003d13e1b4a0f8050e28ba92949e605e
taimoor5356 <mtaimoornaveed@gmail.com>
2025-04-10 22:47:46 +0500

    updated

--- resources/views/frontend/thankyou.blade.php
+++ resources/views/frontend/thankyou.blade.php
@@ -3,6 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-T9TPPJK3');</script>
+    <!-- End Google Tag Manager -->
+
     <title>Thank You</title>
     <style>
         body {
@@ -40,6 +49,10 @@
     </style>
 </head>
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T9TPPJK3"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <a href="https://bkonline.uk" class="header">
         <img src="https://bkonline.uk/public/images/bko-black-logo.png" height="150px" width="150px" alt="">
     </a>
