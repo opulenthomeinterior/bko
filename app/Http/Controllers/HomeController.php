fa8021f58fae0531f44b91ead17051b6931dde5c
taimoor5356 <mtaimoornaveed@gmail.com>
2025-04-11 11:08:38 +0500

    updated

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -895,7 +895,7 @@ class HomeController extends Controller
     }
 
     public function doorsReplacement() {
-        $styles = Style::where('status', 'active')->get();
+        $styles = Style::where('status', '1')->get();
         // foreach ($styles as $style) {
         //     $colourIds = Product::where('parent_category_id', 1)->where('style_id', $style->id)->where('status', 'active')->groupBy('colour_id')->pluck('colour_id');
         //     $style['colours'] = $colourIds;
