a6bc9cad7b4e321f9030fcee81dd5050165ba015
taimoor5356 <mtaimoornaveed@gmail.com>
2025-05-13 10:57:43 +0500

    updated

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1662,14 +1662,15 @@ use App\Models\Style;
                         if (response.success == true) {
                             $('.colour-item').each(function () { 
                                 var colourId = $(this).attr('data-colour-id'); // Get the value
-                                // colourId = parseInt(colourId, 10);
-                                console.log(response, colourId);
+                                colourId = parseInt(colourId, 10);
+                                response.colours = response.colours.map(Number);
 
+                                console.log(response.colours, colourId);
                                 $(this).attr('checked', false); // Uncheck the radio button
                                 if (colourId) {
-                                    console.log(response.colours.includes(colourId.toString()));
+                                    console.log(response.colours.includes(colourId));
                                     // Add your comparison logic here, e.g.:
-                                    if (response.colours.includes(colourId.toString())) {
+                                    if (response.colours.includes(colourId)) {
                                         // Perform an action if `colourId` matches
                                         $(this).attr('disabled', false); // Enable this element
                                         $(this).siblings('.textcolour').css("color", "black"); // Change text color
