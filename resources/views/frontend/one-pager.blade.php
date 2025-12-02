796863f5caae77033402cfa323793e05c36fa806
taimoor5356 <mtaimoornaveed@gmail.com>
2025-12-03 01:58:17 +0500

    updated

--- resources/views/frontend/one-pager.blade.php
+++ resources/views/frontend/one-pager.blade.php
@@ -388,7 +388,7 @@
   <!-- navigation & pagination (optional) -->
   <div class="swiper-button-next"></div>
   <div class="swiper-button-prev"></div>
-  <div class="swiper-pagination"></div>
+  <!-- <div class="swiper-pagination"></div> -->
 </div>
 
     </div>
