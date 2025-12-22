b1bb93c40fa7d20d69d6384f7b10584fc53420e3
taimoor5356 <mtaimoornaveed@gmail.com>
2025-12-23 00:10:50 +0500

    updated

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,14 +2,6 @@
     use App\Models\Category;
     use App\Models\Style;
 ?>
-@section('styles')
-<style>
-    .sticky-active {
-        display: flex !important;
-        justify-content: flex-end !important;
-    }
-</style>
-@endsection
 {{-- HEADER AREA START --}}
 {{--<header class="ltn__header-area ltn__header-5 ltn__header-transparent--- gradient-color-4---">
 
@@ -213,6 +205,18 @@
                                             <sup class="calculateProductsQuantityBottom">0</sup>
                                         </span>
                                         <h6><span>Your Cart</span> <span class="calculateProductsPriceBottom">$0.00</span></h6>
+                                    </a>
+                                </div>
+                            </li>
+                            <li>      
+                                <!-- Mobile Menu Button -->
+                                <div class="mobile-menu-toggle d-lg-none">
+                                    <a href="#ltn__utilize-mobile-menu" class="ltn__utilize-toggle">
+                                        <svg viewBox="0 0 800 600">
+                                            <path d="M300,220 C300,220 520,220 540,220 C740,220 640,540 520,420 C440,340 300,200 300,200" id="top"></path>
+                                            <path d="M300,320 L540,320" id="middle"></path>
+                                            <path d="M300,210 C300,210 520,210 540,210 C740,210 640,530 520,410 C440,330 300,190 300,190" id="bottom" transform="translate(480, 320) scale(1, -1) translate(-480, -318) "></path>
+                                        </svg>
                                     </a>
                                 </div>
                             </li>
