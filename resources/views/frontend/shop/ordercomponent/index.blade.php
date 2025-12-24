bd18321ee5fc20cbe5aa18519df65af3e087bd7e
taimoor5356 <mtaimoornaveed@gmail.com>
2025-12-25 04:10:47 +0500

    updated

--- resources/views/frontend/shop/ordercomponent/index.blade.php
+++ resources/views/frontend/shop/ordercomponent/index.blade.php
@@ -84,7 +84,7 @@
                 <div class="ltn__gallery-item filter_category_3 col-lg-4 col-sm-6 col-12" style="height: 400px; position: absolute; left: 0%; top: 0px;">
                     <div class="ltn__gallery-item-inner" style="height: 400px">
                         <div class="ltn__gallery-item-img d-flex justify-content-center align-items-center" style="height: 400px">
-                            <a href="{{ route('ordercomponentbyname', [$component->slug]) }}" data-rel="lightcase:myCollection">
+                            <a href="{{ route('ordercomponentbyname', [$component->slug]) }}">
                                 <img src="{{ $component->image_path ? asset('imgs/categories/' . $component->image_path) : asset('images/no-image-available.jpg') }}" alt="Image">
                             </a>
                         </div>
